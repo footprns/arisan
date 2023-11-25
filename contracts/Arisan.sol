09fe7b9915cf80434cbe6f402f380c26660150fa
Iman Kurniawan <imankurniawan@gmail.com>
2023-11-25 18:03:34 +0700

    add simple

--- contracts/Arisan.sol
+++ contracts/Arisan.sol
@@ -4,4 +4,9 @@ pragma solidity <=0.8.13;
 
 contract Arisan {
     address payable[] public owners;
+
+    constructor(address payable[] memory _owners) {
+        require(_owners.length = 3, "Three owners are required");
+        owners = _owners;
+    }
 }
